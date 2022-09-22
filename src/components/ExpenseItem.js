62c2b5ca40a6677d62215b39b2210e741efcf1b4
Dennis Littlefield <littlefield.dennis@outlook.com>
2022-09-22 19:16:25 -0400

    added props to expenseitem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import './ExpenseItem.css'
 
-function ExpenseItem()  {
+function ExpenseItem(props)  {
     const expenseDate = new Date(2015, 3, 25);
     const expenseTitle = 'Expense';
     const expenseAmount = 300.00;
 
   return (
       <div className='expense-item'>
-          <div>{expenseDate.toISOString()}</div>
+          <div>{props.date.toISOString()}</div>
           <div className='expense-item__description'>
-              <h2>{expenseTitle}</h2>
+              <h2>{props.title}</h2>
           </div>
-          <div className='expense-item__price'>${expenseAmount}</div>
+          <div className='expense-item__price'>${props.amount}</div>
           
     </div>
   )
