4db3514911ba7956f308fe60b1d0759401b891f3
Dennis Littlefield <littlefield.dennis@outlook.com>
2022-09-27 15:29:12 -0400

    split components into multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
-import './ExpenseItem.css'
 
-function ExpenseItem(props)  {
-    const expenseDate = new Date(2015, 3, 25);
-    const expenseTitle = 'Expense';
-    const expenseAmount = 300.00;
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
 
   return (
-      <div className='expense-item'>
-          <div>{props.date.toISOString()}</div>
-          <div className='expense-item__description'>
-              <h2>{props.title}</h2>
-          </div>
-          <div className='expense-item__price'>${props.amount}</div>
-          
+    <div className='expense-item'>
+      <ExpenseDate date={props.date } />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+      </div>
+      <div className='expense-item__price'>${props.amount}</div>
+
     </div>
   )
 }
